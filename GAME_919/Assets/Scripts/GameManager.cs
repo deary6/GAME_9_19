8f553b3f51d9eeefe13c438331329e4ab27aac1c
deary6 <55227115+deary6@users.noreply.github.com>
2019-10-24 14:58:55 +0800

    程式

---
+++ GAME_919/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;    // 引用 音頻 API
+
+public class GameManager : MonoBehaviour
+{
+    // 定義欄位 (宣告變數)
+    // 修飾詞 類型 名稱 結尾
+    // public 公開 private 私人
+     public AudioMixer mixer;
+
+    // 定義方法 (宣告函式)
+    // 修飾詞 類型 名稱 (參數) { 敘述 }
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+}
