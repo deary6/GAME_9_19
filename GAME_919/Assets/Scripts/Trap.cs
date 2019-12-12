0caa4fb19ed81e1df434a13f99c9dd0c59992f71
deary6 <kevin0975821@gmail.com>
2019-12-12 14:54:37 +0800

    陷阱傷害

---
+++ GAME_919/Assets/Scripts/Trap.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [Header("傷害值"), Range(1, 99)]
+    public int damage = 10;
+}
+
---
+++ GAME_919/Assets/Scripts/playst.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class playst : MonoBehaviour
+{
+    public int hp = 100;        // 血量
+    public Slider hpSlider;     // 血量滑桿
+
+    // 觸發事件：碰到勾選 IsTrigger 物件會執行一次
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "陷阱")    // 如果 碰到.標籤 等於 "陷阱"
+        {
+            int d = other.GetComponent<Trap>().damage;  // 取得元件<泛型>().成員
+            hp -= d;               // 血量 扣 10
+            hpSlider.value = hp;    // 血量滑桿.值 = 血量
+
+
+
+            
+
+
+                }
+    }
+}
