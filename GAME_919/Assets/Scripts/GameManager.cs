60043f174bc93754976e1f906e08e4cacefb4fb9
deary6 <kevin0975821@gmail.com>
2019-12-26 15:34:27 +0800

    網頁版

--- GAME_919/Assets/Scripts/GameManager.cs
+++ GAME_919/Assets/Scripts/GameManager.cs
@@ -40,7 +40,7 @@ public class GameManager : MonoBehaviour
         //yield return new WaitForSeconds(1);       //等待秒數(秒數)
         //print("測試 2");
 
-        AsyncOperation ao = SceneManager.LoadSceneAsync("AK47場景");  //取得場景資訊
+        AsyncOperation ao = SceneManager.LoadSceneAsync("遊戲場景");  //取得場景資訊
         ao.allowSceneActivation = false;                           //取消載入場景
 
 
@@ -57,11 +57,18 @@ public class GameManager : MonoBehaviour
                 ao.allowSceneActivation = true;
             }
         }
-        
 
-
-
-       
-      
     }
+        public void Replay()
+        {
+            SceneManager.LoadScene("介面");
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+
+
 }
--- GAME_919/Assets/Scripts/playst.cs
+++ GAME_919/Assets/Scripts/playst.cs
@@ -14,6 +14,11 @@ public class playst : MonoBehaviour
     public Text textTime;
     public float gameTime;
 
+    [Header("結束畫布")]
+    public GameObject final;
+    public Text textBest;
+    public Text textCurrent;
+
     // 觸發事件：碰到勾選 IsTrigger 物件會執行一次
     private void OnTriggerEnter(Collider other)
     {
@@ -22,6 +27,7 @@ public class playst : MonoBehaviour
             int d = other.GetComponent<Trap>().damage;  // 取得元件<泛型>().成員
             hp -= d;               // 血量 扣 10
             hpSlider.value = hp;    // 血量滑桿.值 = 血量
+            if (hp <= 0) Dead();
         }
 
         if (other.tag == "雞腿")
@@ -33,13 +39,18 @@ public class playst : MonoBehaviour
 
         if (other.name == "終點" && ChickenCount == ChickenTotal)
         {
-            print("過關");
+            GameOver();
         }
 
     }
 
     private void Start()
     {
+        if (PlayerPrefs.GetFloat("最佳時間") == 0)
+        {
+            PlayerPrefs.SetFloat("最佳時間", 99999);
+        }
+
         ChickenTotal = GameObject.FindGameObjectsWithTag("雞腿").Length;
         textChicken.text = "雞腿數量:0 / " + ChickenTotal;
     }
@@ -50,7 +61,19 @@ public class playst : MonoBehaviour
             int d = other.GetComponent<Trap>().damage;  // 取得元件<泛型>().成員
             hp -= d;               // 血量 扣 10
             hpSlider.value = hp;    // 血量滑桿.值 = 血量
+            if (hp <= 0) Dead();
         }
+    }
+
+    private void Dead()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+        textBest.text = "BEST : " + PlayerPrefs.GetFloat("最佳時間").ToString("F2");
+        Cursor.lockState = CursorLockMode.None;
+
+        GetComponent<FPSControllerLPFP.FpsControllerLPFP>().enabled = false;
+        enabled = false;
     }
 
     private void Update()
@@ -63,4 +86,21 @@ public class playst : MonoBehaviour
         gameTime += Time.deltaTime;
         textTime.text = gameTime.ToString("F2");
     }
+
+    private void GameOver()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+
+        if (gameTime < PlayerPrefs.GetFloat("最佳時間"))
+        {
+            PlayerPrefs.SetFloat("最佳時間", gameTime);
+        }
+
+        textBest.text = "BEST : " + PlayerPrefs.GetFloat("最佳時間").ToString("F2");
+        Cursor.lockState = CursorLockMode.None;
+    }
+
 }
+
+   
